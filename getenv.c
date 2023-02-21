1633063d4a22b90a041b3df58770590faa3d8b86
Lilian-banini <lilianbanini319@gmail.com>
2023-02-21 04:24:06 -0800

    task9

--- getenv.c
+++ getenv.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 
 /**
+ * by liolian and josh
  * get_environ - returns the string array copy of our environ
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
